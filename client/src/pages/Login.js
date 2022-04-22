f28ef773a208561018bbde999ebc4ff5b814c65c
Peter Gonzalez <ptr.jgz@gmail.com>
2022-04-22 15:33:33 -0400

    added state to login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,19 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { Typography, TextField, Button } from "@mui/material/";
+// import Success from "./Success";
 
 function Login() {
+  const [user, setUser] = useState({});
+
+  const handleSubmit = () => {
+    console.log(user);
+  };
+
+  const handleChange = (event) => {
+    setUser({ ...user, [event.target.name]: event.target.value });
+  };
+
   return (
     <section style={{ width: "15%", margin: "auto" }}>
       <div style={{ textAlign: "center" }}>
         <Typography>Tijerino</Typography>
         <Typography>Cakes &amp; Pastries</Typography>
       </div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <Typography>Username</Typography>
-        <TextField size="small" fullWidth />
+        <TextField
+          name="username"
+          size="small"
+          fullWidth
+          onChange={(event) => {
+            handleChange(event);
+          }}
+        />
         <Typography>Password</Typography>
-        <TextField size="small" fullWidth />
-        <Button variant="contained">Login</Button>
+        <TextField
+          name="password"
+          size="small"
+          fullWidth
+          onChange={(event) => {
+            handleChange(event);
+          }}
+        />
+        <Button variant="contained" type="submit">
+          Login
+        </Button>
       </form>
     </section>
   );
